cc8725673af909f6f0516d39e022fca62a146be0
wotupfoo <edward@edwardandalison.com>
2026-02-28 23:44:29 -0800

    Button and Axis tests - PASS

--- customJoystick/customJoystick.ino
+++ customJoystick/customJoystick.ino
@@ -16,26 +16,25 @@ void setup() {
 }
 
 void loop() {
-  joy.buttons(1 | 2);
-  joy.axis(0, 0);
-  joy.axis(1, 0);
-  joy.axis(2, 0);
-  joy.axis(3, 0);
-  joy.axis(4, 0);
-  joy.axis(5, 0);
-  joy.axis(6, 0);
-  joy.axis(7, 0);
+  CompositeSerial.print("Updating button");
+  for( uint8_t i = 1; i < joy.getNumButtons()+1; i++ ) {
+    CompositeSerial.print(i);
+    CompositeSerial.print(" ");
+    joy.button(i, false);
+  }
+  CompositeSerial.println();
+  for( uint8_t i = 0; i < joy.getNumAxis(); i++ ) {
+    CompositeSerial.print("Updating axis");
+    CompositeSerial.println(i);
+    joy.axis(i, 0);
+  }
   joy.send(); 
   delay(500);
-  joy.buttons(0);
-  joy.axis(0, 1023);
-  joy.axis(1, 1023);
-  joy.axis(2, 1023);
-  joy.axis(3, 1023);
-  joy.axis(4, 1023);
-  joy.axis(5, 1023);
-  joy.axis(6, 1023);
-  joy.axis(7, 1023);
+
+  joy.buttons(0xFFFFFFFF);
+  for( uint8_t i = 0; i < joy.getNumAxis(); i++ ) {
+    joy.axis(i, 1023);
+  }
   joy.send();
   CompositeSerial.println("I am a customJoystick and Composite Serial device using OpenComposite_STM32F1 library and Arudino!");
   CompositeSerial.println("I show up at a Joystick device (see joy.cpl Windows Control Panel app) and open a serial terminal on the COM port to see!");
